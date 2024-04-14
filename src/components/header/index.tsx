b280898be093e7a6d7d7234e593f7b43968df744
Wevison Ramalho Silva <wevison.silva@altoqi.com.br>
2024-04-14 09:11:09 -0300

    feat: add logo image.

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,5 +1,11 @@
 import styles from './style.module.css';
 
+import logoIgniteFeed from '../../assets/logo-ignite-feed.svg';
+
 export function Header() {
-  return <h2 className={styles.header}>Ignite Feed</h2>;
+  return (
+    <header className={styles.header}>
+      <img src={logoIgniteFeed} alt="Logo Ignite Feed" />
+    </header>
+  );
 }
